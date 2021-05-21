b300bd159ab467d716070610e47bd88eeb56aae0
William Rocha <william.bueno.rocha@gmail.com>
2021-05-21 19:32:35 -0300

    hr-payroll implementation mock

---
+++ hr-payroll/src/main/java/com/dev/worker/hrpayroll/resources/PaymentResource.java
@@ -0,0 +1,27 @@
+package com.dev.worker.hrpayroll.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.dev.worker.hrpayroll.entities.Payment;
+import com.dev.worker.hrpayroll.services.PaymentService;
+
+@RestController
+@RequestMapping(value = "/payments")
+public class PaymentResource {
+	
+	@Autowired
+	private PaymentService service;
+	
+	@GetMapping(value = "/{workerId}/days/{days}")
+	public ResponseEntity<Payment> getPaymEntity(@PathVariable Long workerId, @PathVariable Integer days){
+		Payment payment = service.getPayment(workerId, days);
+		return ResponseEntity.ok(payment);
+	}
+	
+
+}
