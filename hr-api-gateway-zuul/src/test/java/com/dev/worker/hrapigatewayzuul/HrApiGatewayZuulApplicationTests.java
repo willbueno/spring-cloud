851c57a52dd4db47776412a8d298800c8104eb17
William Rocha <william.bueno.rocha@gmail.com>
2021-05-29 22:09:38 -0300

    Project hr-api-gateway-zuul created

---
+++ hr-api-gateway-zuul/src/test/java/com/dev/worker/hrapigatewayzuul/HrApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.worker.hrapigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrApiGatewayZuulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
